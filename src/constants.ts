06409b8049e33536a8876fbcac5d6a638890b459
Andrew Rowley <andrew@internetdrew.com>
2026-09-21 16:10:11 -0400

    add initial ui

---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+export const title = "Interfaces about nothing.";
+export const description =
+  "A collection of interactive interfaces and artifacts inspired by Seinfeld episodes, characters, and their everyday absurdities.";
